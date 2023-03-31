30a79300353c25536efde40a580bb0cf5c0b2e6a
HeatherCaudill <heather-caudill@outlook.com>
2023-03-31 12:15:03 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace sd1_kc2
+{
+    internal class Program
+    {
+
+    }
+    public class Transportation
+
+    {
+        public string Transmission;
+        { 
+            Automatic;
+            Manual;
+        }
+        public string Records;
+        {
+            Repairs;
+            Books;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("How many records do you want to add? ");
+        var numberOfRecords = int.Parse(Console.ReadLine());
+
+        var recordList = new List<Transportation>();
+        for (int i = 0; i < numberOfRecords; i++)
+        {
+            // In this loop, populate the object's properties using Console.ReadLine()
+            var Transportation = new Transportation();
+
+            Console.WriteLine("Enter the value for ");
+            Transportation.= Console.ReadLine();
+            recordList.Add(Transportation);
+        }
+
+        // Print out the list of records using Console.WriteLine()
+    }
+}
